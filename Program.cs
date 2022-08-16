db915aaaee64010eec7ab54afeeb42e77cc8b32f
ibrahimcakmakk <ibrahim.cakmak@stu.fsm.edu.tr>
2022-08-16 17:38:33 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+
+    static void Main(string[] args){
+        Console.Write("lütfen bir sayı giriniz");
+        int sayi = int.Parse(Console.ReadLine());
+        int sayac = 1;
+        int toplam = 0;
+
+        while (sayac <=sayi)
+        {
+            toplam += sayac;
+            sayac++;
+        }
+        Console.WriteLine(toplam/sayi);
+
+        char character = 'a';
+        while(character<'z')
+        {
+            Console.Write(character);
+            character++;
+        }
+        Console.Write("***** Foreach *****");
+        string[] arabalar = {"BMW","Ford","Toyota","Nissan"};
+
+        foreach (var item in arabalar)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
